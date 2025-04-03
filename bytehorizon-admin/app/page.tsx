41c6771c7823d1d9925203c06bcd1681f5483585
Justin Willoughby <justin@clerk.dev>
2025-04-03 11:13:57 -0500

    Add getToken to ensure it works in iframe

--- bytehorizon-admin/app/page.tsx
+++ bytehorizon-admin/app/page.tsx
@@ -1,10 +1,22 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, UserButton, useAuth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const { getToken } = await auth();
+  
   return (
     <main>
       <SignedIn>
         <UserButton />
+        <button onClick={async () => {
+          const token = await getToken();
+          console.log(token);
+          const tokenDiv = document.getElementById("token");
+          if (tokenDiv) {
+            tokenDiv.textContent = token;
+          }
+        }}>Get Token</button>
+        <div id="token"></div>
       </SignedIn>
       <SignedOut>
         <SignInButton />
